4a01e2373f2c135a85376b668ea0c8c9b4df0655
piotrsiwek <59854137+piotrsiwek@users.noreply.github.com>
2024-03-15 10:28:21 +0100

    working count function but only with go run

--- cmd/main.go
+++ cmd/main.go
@@ -1,44 +1,43 @@
 package main
 
 import (
+	"html/template"
+	"io"
+
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 )
 
 type Templates struct {
-	templates *template.Templates
-
-
-	}
-
-
-func (t *Templates) Render(w echo.ResponseWriter, name string, data interface{}, c echo	Context) error {
-	return t.templates.ExecuteTemplate(w, name, data)
+	templates *template.Template
 }
 
-func newTemplate () *Templates {
+func newTemplate() *Templates {
 	return &Templates{
 		templates: template.Must(template.ParseGlob("views/*.html")),
-    }
+	}
+}
+
+func (t *Templates) Render(w io.Writer, name string, data interface{}, c echo.Context) error {
+	return t.templates.ExecuteTemplate(w, name, data)
 }
 
 type Count struct {
 	Count int
-
 }
-func main() {
-e:
-	echo.New()
-	e.use(middleware.Logger())
 
-	count = Count{Count: 0}
-	e.Render= newTemplate()
+func main() {
+	e := echo.New()
+	e.Use(middleware.Logger())
+
+	count := Count{Count: 0}
+	e.Renderer = newTemplate()
 
 	e.GET("/", func(c echo.Context) error {
 		count.Count++
-			return c.Render(200,"index.html",count)
-})
+		return c.Render(200, "index", count)
+	})
 
-    e.Logger.Fatal(e.Start(":8080"))
+	e.Logger.Fatal(e.Start(":42069"))
 
 }
